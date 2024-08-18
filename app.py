2a670ad6312458f81ca6c4fae2857e0666137a1d
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-18 16:59:55 +0530

    upd

--- app.py
+++ app.py
@@ -11,3 +11,4 @@ def loading_data():
     df = pd.read_csv('../assets/healthcare_dataset.csv')
     df["Billing Amount"] = pd.to_numeric(df["Billing Amount"], errors='coerce')
     df["Date of Admission"] = pd.to_datetime(df["Date of Admission"])
+    df["YearMonth"] = df["Date of Admission"].dt.to_period("M")
