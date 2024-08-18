59707d69202542dabf640e6df652f7e79c3d116a
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-18 20:35:30 +0530

    upd

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 # loading data
 
 def loading_data():
-    df = pd.read_csv('../assets/healthcare_dataset.csv')
+    df = pd.read_csv('assets/healthcare_dataset.csv')
     df["Billing Amount"] = pd.to_numeric(df["Billing Amount"], errors ='coerce')
     df["Date of Admission"] = pd.to_datetime(df["Date of Admission"])
     df["YearMonth"] = df["Date of Admission"].dt.to_period("M")
@@ -24,8 +24,10 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
     dbc.Row([
-        dbc.Col(html.H1("HealthCare Dashboard"), width=15, className="text-centre")
-    ])
+        dbc.Col(html.H1("HealthCare Dashboard"), width=15, className="text-centre my-5")
+    ]),
+    # hospital statistics
+    dbc.Row([dbc.Col(html.Div(f"Total Patient Records:"))])
 ])
 
 
