110a2761dc94bd03925267b31eed51f020367e5c
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-08-18 17:04:04 +0530

    upd

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ import pandas as pd
 
 def loading_data():
     df = pd.read_csv('../assets/healthcare_dataset.csv')
-    df["Billing Amount"] = pd.to_numeric(df["Billing Amount"], errors='coerce')
+    df["Billing Amount"] = pd.to_numeric(df["Billing Amount"], errors ='coerce')
     df["Date of Admission"] = pd.to_datetime(df["Date of Admission"])
     df["YearMonth"] = df["Date of Admission"].dt.to_period("M")
+    return df
+
+df = loading_data()
+
+# creating web app
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+    
